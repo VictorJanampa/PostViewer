fddd7042ffe6d9452d787a61fb425f6951009703
VictorJanampa <torrviccc@gmail.com>
2022-04-23 14:54:30 -0500

    improvement: removed logs and unused imports

--- app/src/main/java/com/example/postviewer/commentview/CommentListViewModel.kt
+++ app/src/main/java/com/example/postviewer/commentview/CommentListViewModel.kt
@@ -8,8 +8,6 @@ import com.example.postviewer.network.Post
 import com.example.postviewer.network.PostApi
 import kotlinx.coroutines.launch
 
-enum class ApiStatus { LOADING, ERROR, DONE }
-
 class CommentListViewModel (post: Post, app: Application) : AndroidViewModel(app){
 
     private val _selectedPost = MutableLiveData<Post>()
@@ -17,9 +15,6 @@ class CommentListViewModel (post: Post, app: Application) : AndroidViewModel(app
     init {
         _selectedPost.value = post
     }
-    private val _status = MutableLiveData<ApiStatus>()
-    val status: LiveData<ApiStatus>
-        get() = _status
 
     private val _posts = MutableLiveData<List<Comment>>()
     val posts: LiveData<List<Comment>>
@@ -30,12 +25,9 @@ class CommentListViewModel (post: Post, app: Application) : AndroidViewModel(app
     }
     fun getComments() {
         viewModelScope.launch {
-            _status.value = ApiStatus.LOADING
             try {
                 _posts.value = _selectedPost.value?.let { PostApi.retrofitService.getPostComments(it.id) }
-                _status.value = ApiStatus.DONE
             } catch (e: Exception) {
-                _status.value = ApiStatus.ERROR
                 _posts.value = ArrayList()
             }
         }
--- app/src/main/java/com/example/postviewer/postview/PostListViewModel.kt
+++ app/src/main/java/com/example/postviewer/postview/PostListViewModel.kt
@@ -13,13 +13,7 @@ import com.example.postviewer.network.PostApi
 import kotlinx.coroutines.NonDisposableHandle.parent
 import kotlinx.coroutines.launch
 
-enum class ApiStatus { LOADING, ERROR, DONE }
-
 class PostListViewModel(application: Application) : ViewModel(){
-
-    private val _status = MutableLiveData<ApiStatus>()
-    val status: LiveData<ApiStatus>
-        get() = _status
 
     private val _posts = MutableLiveData<List<Post>>()
     val posts: LiveData<List<Post>>
@@ -29,7 +23,7 @@ class PostListViewModel(application: Application) : ViewModel(){
     val navigateToSelectedProperty: MutableLiveData<Post?>
         get() = _navigateToSelectedItem
 
-    val database = PostDatabase.getInstance(application).postDatabaseDao
+    private val database = PostDatabase.getInstance(application).postDatabaseDao
 
     init {
         fetchPosts()
@@ -37,20 +31,15 @@ class PostListViewModel(application: Application) : ViewModel(){
 
     fun fetchPosts() {
         viewModelScope.launch {
-            _status.value = ApiStatus.LOADING
             try {
                 _posts.value = PostApi.retrofitService.getPosts().also {
                     for (post in it){
                         database.insertPost(post)
                     }
                 }
-                _status.value = ApiStatus.DONE
             } catch (e: Exception) {
-                _status.value = ApiStatus.ERROR
-                e.message?.let { Log.i("Andrio", it) }
                 val data=database.getAllPosts()
-                Log.i("Andrio", data.toString())
-                _posts.value = data ?: ArrayList()
+                _posts.value = data
             }
         }
     }
